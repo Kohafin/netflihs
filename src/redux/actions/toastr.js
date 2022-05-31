84f425230dc40749791ceb12ee1b736807cb2c58
dornachmias <dor@nachmias.me>
2022-05-31 21:56:25 +0300

    refactor

--- src/redux/actions/toastr.js
+++
@@ -1,7 +0,0 @@
-import { actions } from 'react-redux-toastr';
-
-export const toastrSuccess = (title, message) =>
-actions.add({ title, message, type: 'success' });
-
-export const toastrError = (title, message) =>
-actions.add({ title, message, type: 'error' });
--- src/redux/middleware/log.js
+++
@@ -1,7 +0,0 @@
-const log = ({ dispatch, getState }) => next => action => {
-    console.log('ACTION: ' + action.type, action)
-
-    return next(action)
-}
-
-export default log
